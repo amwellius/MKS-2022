2ad4fecfa089fc98a87c15f2775077b791d3d8da
amwellius <xkosik09@vutbr.cz>
2022-10-04 12:22:05 +0200

    add cv02

---
+++ 2 4.10/cv02/Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ C:/Temp/221056/1\ 20.9/cv01/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Temp/221056/1\ 20.9/cv01/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/Temp/221056/1\ 20.9/cv01/Include/core_cm0.h \
+ C:/Temp/221056/1\ 20.9/cv01/Include/core_cmInstr.h \
+ C:/Temp/221056/1\ 20.9/cv01/Include/core_cmFunc.h \
+ C:/Temp/221056/1\ 20.9/cv01/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+C:/Temp/221056/1\ 20.9/cv01/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+C:/Temp/221056/1\ 20.9/cv01/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+C:/Temp/221056/1\ 20.9/cv01/Include/core_cm0.h:
+C:/Temp/221056/1\ 20.9/cv01/Include/core_cmInstr.h:
+C:/Temp/221056/1\ 20.9/cv01/Include/core_cmFunc.h:
+C:/Temp/221056/1\ 20.9/cv01/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
---
+++ 2 4.10/cv02/Debug/Src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32f0xx.c 
+
+OBJS += \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32f0xx.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32f0xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F030x8 -DSTM32F030R8Tx -DSTM32 -DSTM32F0 -DNUCLEO_F030R8 -c -I../Inc -I"C:/Temp/221056/1 20.9/cv01/Device/ST/STM32F0xx/Include" -I"C:/Temp/221056/1 20.9/cv01/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32f0xx.d ./Src/system_stm32f0xx.o ./Src/system_stm32f0xx.su
+
+.PHONY: clean-Src
+
